733ec66214b9529ee9873f9d7b06f08c3ed3c007
Junhang Yin <yinjunhang@outlook.com>
2026-01-11 01:07:24 +0800

    Stop tracking build folder

--- build/kernel/shell/CMakeFiles/shell.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/shell.dir/shmsg.c.obj"
-  "CMakeFiles/shell.dir/shmsg.c.obj.d"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/shell.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/kernel/sys/CMakeFiles/sys.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sys.dir/prt_sys.c.obj"
-  "CMakeFiles/sys.dir/prt_sys.c.obj.d"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/sys.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
